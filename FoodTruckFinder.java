6e96a51ccad53d36c18f0b69f4242bc8633223b4
j <himynameisjoeq@gmail.com>
2018-12-02 21:16:41 -0700

    #002 fix

--- FoodTruckFinder.java
+++ FoodTruckFinder.java
@@ -1,67 +1,83 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.time.ZoneId;
-import java.text.SimpleDateFormat;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FoodTruckFinder {
-	public ArrayList<FoodTruck> FindTrucks() {
-   		try {
-   			//var declaration
-			ArrayList<FoodTruck> trucks = new ArrayList<FoodTruck>();
-			String line;
-			int index;
 
-			//prepare time vars (always assume PST)
-			LocalTime currentTime = LocalTime.now(ZoneId.of("America/Los_Angeles"));
-			Calendar c = Calendar.getInstance(TimeZone.getTimeZone("PST"));
-			SimpleDateFormat dateFormat = new SimpleDateFormat("EEEEE");
-			String day = dateFormat.format(c.getTime());
+    public static void main(String[] args) {
+	// write your code here
+        FTFSearch search = new FTFSearch();
+        int index = 0;
+        String input;
+        Boolean inputting;
+        Scanner in = new Scanner(System.in);
 
-			//connect and start loop
-			URL url = new URL("https://data.sfgov.org/resource/bbb8-hzi6.json");
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			while ((line = rd.readLine()) != null) {
-				//check if open. compare day
-				index = line.indexOf("\"dayofweekstr\"");
-				if (index == -1) continue;
-				if (!day.equals(line.substring(index + 16, line.indexOf("\",", index + 16)))) continue;
+        System.out.println("~San Francisco Food Truck Finder~");
 
-				//compare closing
-				index = line.indexOf("\"end24\"");
-				if (index == -1) continue;
-				if ("24:00".equals(line.substring(index + 9, line.indexOf("\",", index + 9)))) {} //base case for midnight
-				else if (currentTime.isAfter(LocalTime.parse(line.substring(index + 9, line.indexOf("\",", index + 9)), DateTimeFormatter.ofPattern("HH:mm")))) continue;
+        System.out.println("Searching and sorting...");
+        ArrayList<FoodTruck> trucks = search.FindTrucks();
+        System.out.println("Success!\n");
 
-				//compare opening
-				index = line.indexOf("\"start24\"");
-				if (index == -1) continue;
-				if (currentTime.isBefore(LocalTime.parse(line.substring(index + 11, line.indexOf("\",", index + 11)), DateTimeFormatter.ofPattern("HH:mm")))) continue;
+        //central loop
+        while(true)
+        {
+            System.out.format("%-3c %-40s %-1s %n", '#', "Name", "Address");
+            for (int i = index; i < index + 10; i++)
+            {
+                if (i < trucks.size())
+                {
+                    System.out.format("%-3d %-40s %-1s %n", i + 1, trucks.get(i).getName(),  trucks.get(i).getAddress());
+                }
+            }
+            if (index + 10 < trucks.size())
+            {
+                System.out.println("\"next\" to view next page");
+            }
+            if (index > 0)
+            {
+                System.out.println("\"previous\" to view previous page");
+            }
+            System.out.println("\"q\" to quit");
 
-				//confirmed open, now add the truck to results list
-				index = line.indexOf("\"applicant\"");
-				if (index == -1) continue;
-				String name = line.substring(index + 13, line.indexOf("\",", index + 13));
-				index = line.indexOf("\"location\"");
-				if (index == -1) continue;
-				String address = line.substring(index + 12, line.indexOf("\",", index + 12));
-				trucks.add(new FoodTruck(name, address));
-			}
-			rd.close();
-			Collections.sort(trucks, FoodTruck.FoodTruckComparator);
-			return trucks;
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-   		return null;
-	}
+            inputting = true;
+            while(inputting)
+            {
+                input = in.nextLine();
+                if (input.equals("next"))
+                {
+                    if(index + 10 < trucks.size())
+                    {
+                        index = index + 10;
+                        inputting = false;
+                    }
+                    else
+                    {
+                        System.out.println("you are already on the last page");
+                    }
+                }
+                else if (input.equals("previous"))
+                {
+                    if(index > 0)
+                    {
+                        index = index - 10;
+                        inputting = false;
+                    }
+                    else
+                    {
+                        System.out.println("you are already on the first page");
+                    }
+                }
+                else if (input.equals("q"))
+                {
+                    return;
+                }
+                else
+                {
+                    System.out.println("please input a valid command");
+                }
+            }
+            System.out.println("\n~San Francisco Food Truck Finder~");
+        }
+
+    }
 }
-
-// to run:
-// $ javac FoodTruckFinder.java && java FoodTruckFinder
